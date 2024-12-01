7b516cdb3ae9ae2f3002826877d6ca2d73b40c41
kees <keesraijmakers@live.nl>
2024-12-01 14:45:11 +0100

    Adding query functionality and updated Scoreboard & Challenges

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from . import db
 from .models import challenges, studentpoints, student
 
 views = Blueprint('views', __name__)
@@ -15,11 +14,11 @@ def home():
 @views.route('/scoreboard', methods=['GET', 'POST'])
 @login_required
 def scoreboard():
-    Scoreboard = studentpoints.query.all()
-    User = current_user.get_id()
-    return render_template("scoreboard.html", user=current_user, User=User, scoreboards=Scoreboard)
+    Students = student.query.all()
+    Scoreboards = studentpoints.query.all()
+    return render_template("scoreboard.html", user=current_user, students=Students, scoreboards=Scoreboards)
 
 @views.route('/challenge/<id>', methods=['GET', 'POST'])
 def challenge(id):  
     Challenges = challenges.query.all()
-    return render_template("challenge.html", user=current_user, challenge_id=id, challenges=Challenges)
+    return render_template("challenge.html", user=current_user, challengeid=int(id), challenges=Challenges)
